a0cacdd1c68fbf4b552d0e803ca702786b5b6636
syzhang <syzhsy@qq.com>
2020-05-14 01:36:16 +0800

    no message

--- oracle-10.x-plugin/src/main/java/io/skywalking/apm/plugin/jdbc/oracle/Constants.java
+++ oracle-10.x-plugin/src/main/java/io/skywalking/apm/plugin/jdbc/oracle/Constants.java
@@ -42,6 +42,7 @@ public final class Constants {
 		try {
 			System.out.println("log4j初始化");
 			URL fileURL = Constants.class.getClassLoader().getResource("src/main/resource/log4j.properties");
+			System.out.println(fileURL == null);
 			PropertyConfigurator.configure(fileURL.getFile());
 			System.out.println("初始化完毕");
 		} catch (Exception e) {
